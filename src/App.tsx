b805ccf2a132026dc3f2fb3f8e22e685b35785ec
viniciusvilar <61420617+viniciusvilar@users.noreply.github.com>
2022-10-29 09:12:22 -0300

    Configuração inicial de estilo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { GlobalStyle } from "./styles/global";
+
+
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <div className="App">
+      <h1>Hello World</h1>
+      <GlobalStyle/>
+    </div>
   );
 }
 
