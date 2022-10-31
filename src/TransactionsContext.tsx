448a5e0f1103c79238d6b8ed92b39b7788c4e8a2
viniciusvilar <61420617+viniciusvilar@users.noreply.github.com>
2022-10-31 15:56:06 -0300

    Configuração de transação

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -10,11 +10,20 @@ interface Transaction {
     createdAt: string;
 }
 
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>
+
 interface TransactionsProviderProps {
     children: ReactNode
 }
 
-export const TransactionsContext = createContext<Transaction[]>([]);
+interface TransactionsContextData {
+    transactions: Transaction[]
+    createTransaction: (transaction: TransactionInput) => Promise<void>;
+}
+
+export const TransactionsContext = createContext<TransactionsContextData>(
+    {} as TransactionsContextData
+);
 
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
     const [transactions, setTrancactions] = useState<Transaction[]>([])
@@ -24,8 +33,22 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
             .then(response => setTrancactions(response.data.transactions))
     }, []);
 
+    async function createTransaction(transactionInput: TransactionInput) {
+        const response = await api.post('/transactions', {
+            ...transactionInput,
+            createdAt: new Date(),
+        })
+
+        const { transaction } = response.data;
+
+        setTrancactions([
+            ...transactions,
+            transaction
+        ])
+    }
+
     return (
-        <TransactionsContext.Provider value={transactions}>
+        <TransactionsContext.Provider value={{ transactions, createTransaction }}>
             {children}
         </TransactionsContext.Provider>
     )
